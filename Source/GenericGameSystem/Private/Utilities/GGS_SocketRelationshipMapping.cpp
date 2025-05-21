71a29e751e0612e49fed53064b7de2c044357319
luochuanyuewu <luochuanyuewu@gmail.com>
2025-05-21 11:36:03 +0800

    Added socket relationship mapping utilites, allow adjustment for different mesh attached to different socket on different skeletion mash. data-driven.

---
+++ Source/GenericGameSystem/Private/Utilities/GGS_SocketRelationshipMapping.cpp
@@ -0,0 +1,104 @@
+﻿// Copyright 2024 https://yuewu.dev/en  All Rights Reserved.
+
+
+#include "Utilities/GGS_SocketRelationshipMapping.h"
+
+#include "UObject/ObjectSaveContext.h"
+
+bool UGGS_SocketRelationshipMapping::FindSocketAdjustment(const USkeletalMeshComponent* InParentMeshComponent, const UStreamableRenderAsset* InMeshAsset, FName InSocketName,
+                                                          FGGS_SocketAdjustment& OutAdjustment) const
+{
+	if (InParentMeshComponent == nullptr || InMeshAsset == nullptr || InSocketName.IsNone())
+	{
+		return false;
+	}
+	USkeleton* Skeleton = InParentMeshComponent->GetSkeletalMeshAsset()->GetSkeleton();
+	if (!Skeleton)
+	{
+		return false;
+	}
+	FString SkeletonName = Skeleton->GetName();
+	
+	for (const FGGS_SocketRelationship& Relationship : Relationships)
+	{
+		UStreamableRenderAsset* Key{nullptr};
+		if (!Relationship.MeshAsset.IsNull())
+		{
+			Key = Relationship.MeshAsset.LoadSynchronous();
+		}
+		if (!Key || Key->GetName() != InMeshAsset->GetName())
+		{
+			continue;
+		}
+		for (int32 i=Relationship.Adjustments.Num() -1; i>=0;i--)
+		{
+			const FGGS_SocketAdjustment& Adjustment = Relationship.Adjustments[i];
+			bool bMatchSkeleton = Adjustment.ForSkeletons.IsEmpty()?true:Adjustment.ForSkeletons.Contains(SkeletonName);
+			if (bMatchSkeleton && Adjustment.SocketName == InSocketName)
+			{
+				OutAdjustment = Adjustment;
+				return true;
+			}
+		}
+	}
+	
+	return false;
+}
+
+bool UGGS_SocketRelationshipMapping::FindSocketAdjustmentInMappings(TArray<TSoftObjectPtr<UGGS_SocketRelationshipMapping>> InMappings,const USkeletalMeshComponent* InParentMeshComponent, const UStreamableRenderAsset* InMeshAsset, FName InSocketName,
+	FGGS_SocketAdjustment& OutAdjustment)
+{
+	for (TSoftObjectPtr<UGGS_SocketRelationshipMapping> Mapping : InMappings)
+	{
+		if (Mapping.IsNull())
+		{
+			continue;
+		}
+		if (const UGGS_SocketRelationshipMapping* LoadedMapping = Mapping.LoadSynchronous())
+		{
+			if (LoadedMapping->FindSocketAdjustment(InParentMeshComponent, InMeshAsset, InSocketName, OutAdjustment))
+			{
+				return true;
+			}
+		}
+	}
+	return false;
+}
+
+#if WITH_EDITORONLY_DATA
+void UGGS_SocketRelationshipMapping::PreSave(FObjectPreSaveContext SaveContext)
+{
+	for (FGGS_SocketRelationship& Relationship : Relationships)
+	{
+		if (Relationship.MeshAsset.IsNull())
+		{
+			Relationship.EditorFriendlyName = TEXT("Invalid!");
+		}else
+		{
+			UStreamableRenderAsset* MeshAsset = Relationship.MeshAsset.LoadSynchronous();
+			Relationship.EditorFriendlyName = MeshAsset->GetName();
+			for (FGGS_SocketAdjustment& Adjustment : Relationship.Adjustments)
+			{
+				if (Adjustment.SocketName == NAME_None)
+				{
+					Adjustment.EditorFriendlyName = "Empty adjustments!";
+				}
+				if (Adjustment.ForSkeletons.IsEmpty())
+				{
+					Adjustment.EditorFriendlyName = Adjustment.SocketName.ToString();
+				}
+				else
+				{
+					FString SkeletonNames;
+					for (const FString&  ForSkeleton: Adjustment.ForSkeletons)
+					{
+						SkeletonNames = SkeletonNames.Append(ForSkeleton);
+					}
+					Adjustment.EditorFriendlyName = FString::Format(TEXT("{0} on {1}"),{Adjustment.SocketName.ToString(),SkeletonNames});
+				}
+			}
+		}
+	}
+	Super::PreSave(SaveContext);
+}
+#endif
---
+++ Source/GenericGameSystem/Public/Utilities/GGS_SocketRelationshipMapping.h
@@ -0,0 +1,74 @@
+﻿// Copyright 2024 https://yuewu.dev/en  All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/DataAsset.h"
+#include "GGS_SocketRelationshipMapping.generated.h"
+
+USTRUCT(BlueprintType)
+struct GENERICGAMESYSTEM_API FGGS_SocketAdjustment
+{
+	GENERATED_BODY()
+
+	/**
+	 * Name of skeletons.
+	 */
+	UPROPERTY(EditAnywhere, Category="GGS")
+	TArray<FString> ForSkeletons;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="GGS")
+	FName SocketName{NAME_None};
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="GGS")
+	FTransform RelativeTransform;
+
+#if WITH_EDITORONLY_DATA
+	UPROPERTY(EditAnywhere, Category="GGS", meta=(EditCondition=false, EditConditionHides))
+	FString EditorFriendlyName;
+#endif	
+};
+
+USTRUCT(BlueprintType)
+struct GENERICGAMESYSTEM_API FGGS_SocketRelationship
+{
+	GENERATED_BODY()
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="GGS")
+	TSoftObjectPtr<UStreamableRenderAsset> MeshAsset;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="GGS",meta=(TitleProperty="EditorFriendlyName"))
+	TArray<FGGS_SocketAdjustment> Adjustments;
+
+#if WITH_EDITORONLY_DATA
+	UPROPERTY(EditAnywhere, Category="GGS", meta=(EditCondition=false, EditConditionHides))
+	FString EditorFriendlyName;
+#endif
+};
+
+
+/**
+ * 
+ */
+UCLASS(BlueprintType)
+class GENERICGAMESYSTEM_API UGGS_SocketRelationshipMapping : public UDataAsset
+{
+	GENERATED_BODY()
+
+public:
+	UFUNCTION(BlueprintCallable,BlueprintPure=False, Category="GGS|Utilities")
+	bool FindSocketAdjustment(const USkeletalMeshComponent* InParentMeshComponent, const UStreamableRenderAsset* InMeshAsset,FName InSocketName,
+								 FGGS_SocketAdjustment& OutAdjustment) const;
+
+	UFUNCTION(BlueprintCallable, Category="GGS|Utilities")
+	static bool FindSocketAdjustmentInMappings(TArray<TSoftObjectPtr<UGGS_SocketRelationshipMapping>> InMappings,const USkeletalMeshComponent* InParentMeshComponent, const UStreamableRenderAsset* InMeshAsset,FName InSocketName,
+								 FGGS_SocketAdjustment& OutAdjustment);
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="GGS",meta=(TitleProperty="EditorFriendlyName"))
+	TArray<FGGS_SocketRelationship> Relationships;
+
+
+#if WITH_EDITORONLY_DATA
+	virtual void PreSave(FObjectPreSaveContext SaveContext) override;
+#endif
+};
