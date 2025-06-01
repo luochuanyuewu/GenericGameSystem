1ee8205cee97a08e7a4b9b08ccf71714ccc47ff4
luochuanyuewu <luochuanyuewu@gmail.com>
2025-06-01 21:53:28 +0800

    foramt code.

--- Source/GenericUISystem/Public/UI/GUIS_GameUIFunctionLibrary.h
+++ Source/GenericUISystem/Public/UI/GUIS_GameUIFunctionLibrary.h
@@ -35,40 +35,42 @@ public:
 	}
 
 	UFUNCTION(BlueprintPure, BlueprintCosmetic, Category="GUIS", meta = (WorldContext = "WidgetContextObject"))
-	static ECommonInputType GetOwningPlayerInputType(const UUserWidget *WidgetContextObject);
+	static ECommonInputType GetOwningPlayerInputType(const UUserWidget* WidgetContextObject);
 
 	UFUNCTION(BlueprintPure, BlueprintCosmetic, Category="GUIS", meta = (WorldContext = "WidgetContextObject"))
-	static bool IsOwningPlayerUsingTouch(const UUserWidget *WidgetContextObject);
+	static bool IsOwningPlayerUsingTouch(const UUserWidget* WidgetContextObject);
 
 	UFUNCTION(BlueprintPure, BlueprintCosmetic, Category="GUIS", meta = (WorldContext = "WidgetContextObject"))
-	static bool IsOwningPlayerUsingGamepad(const UUserWidget *WidgetContextObject);
+	static bool IsOwningPlayerUsingGamepad(const UUserWidget* WidgetContextObject);
 
-	UFUNCTION(BlueprintCallable, BlueprintCosmetic, Category="GUIS")
-	static UCommonActivatableWidget *PushContentToUILayer_ForPlayer(const APlayerController *PlayerController, UPARAM(meta = (Categories = "UI.Layer,GUIS.Layer")) FGameplayTag LayerName,
-																	UPARAM(meta = (AllowAbstract = false)) TSubclassOf<UCommonActivatableWidget> WidgetClass);
+	UFUNCTION(BlueprintCallable, BlueprintCosmetic, Category="GUIS", meta=(DynamicOutputParam="ReturnValue", DeterminesOutputType="WidgetClass"))
+	static UCommonActivatableWidget* PushContentToUILayer_ForPlayer(const APlayerController* PlayerController, UPARAM(meta = (Categories = "UI.Layer,GUIS.Layer"))
+	                                                                FGameplayTag LayerName,
+	                                                                UPARAM(meta = (AllowAbstract = false))
+	                                                                TSubclassOf<UCommonActivatableWidget> WidgetClass);
 
 	// UFUNCTION(BlueprintCallable, BlueprintCosmetic, Category="GUIS")
 	// static void PushStreamedContentToLayer_ForPlayer(const ULocalPlayer* LocalPlayer, UPARAM(meta = (Categories = "UI.Layer")) FGameplayTag LayerName,
 	//                                                  UPARAM(meta = (AllowAbstract = false)) TSoftClassPtr<UCommonActivatableWidget> WidgetClass);
 
-	UFUNCTION(BlueprintCallable, BlueprintCosmetic, Category="GUIS")
-	static void PopContentFromUILayer(UCommonActivatableWidget *ActivatableWidget);
+	UFUNCTION(BlueprintCallable, BlueprintCosmetic, Category="GUIS", meta=(DefaultToSelf="ActivatableWidget"))
+	static void PopContentFromUILayer(UCommonActivatableWidget* ActivatableWidget);
 
 	UFUNCTION(BlueprintCallable, BlueprintCosmetic, Category="GUIS")
-	static ULocalPlayer *GetLocalPlayerFromController(APlayerController *PlayerController);
+	static ULocalPlayer* GetLocalPlayerFromController(APlayerController* PlayerController);
 
 	UFUNCTION(BlueprintCallable, BlueprintCosmetic, Category="GUIS")
-	static UGUIS_GameUILayout *GetGameUILayoutForPlayer(const APlayerController *PlayerController);
+	static UGUIS_GameUILayout* GetGameUILayoutForPlayer(const APlayerController* PlayerController);
 
 	UFUNCTION(BlueprintCallable, BlueprintCosmetic, Category="GUIS")
-	static FName SuspendInputForPlayer(APlayerController *PlayerController, FName SuspendReason);
+	static FName SuspendInputForPlayer(APlayerController* PlayerController, FName SuspendReason);
 
-	static FName SuspendInputForPlayer(ULocalPlayer *LocalPlayer, FName SuspendReason);
+	static FName SuspendInputForPlayer(ULocalPlayer* LocalPlayer, FName SuspendReason);
 
 	UFUNCTION(BlueprintCallable, BlueprintCosmetic, Category="GUIS")
-	static void ResumeInputForPlayer(APlayerController *PlayerController, FName SuspendToken);
+	static void ResumeInputForPlayer(APlayerController* PlayerController, FName SuspendToken);
 
-	static void ResumeInputForPlayer(ULocalPlayer *LocalPlayer, FName SuspendToken);
+	static void ResumeInputForPlayer(ULocalPlayer* LocalPlayer, FName SuspendToken);
 
 	/**
 	 * Returns the typed item in the owning list view that this entry is currently assigned to represent.
@@ -77,8 +79,8 @@ public:
 	 * @param UserObjectListEntry Note: Visually not transmitted, but this defaults to "self". No need to hook up if calling internally.
 	 */
 	UFUNCTION(BlueprintPure, Category="GUIS",
-			  meta = (DeterminesOutputType = "DesiredClass", DynamicOutputParam = "ReturnValue", DefaultToSelf = UserObjectListEntry, DisplayName = "Get List item Object"))
-	static UObject *GetTypedListItem(TScriptInterface<IUserObjectListEntry> UserObjectListEntry, TSubclassOf<UObject> DesiredClass);
+		meta = (DeterminesOutputType = "DesiredClass", DynamicOutputParam = "ReturnValue", DefaultToSelf = UserObjectListEntry, DisplayName = "Get List item Object"))
+	static UObject* GetTypedListItem(TScriptInterface<IUserObjectListEntry> UserObjectListEntry, TSubclassOf<UObject> DesiredClass);
 
 	/**
 	 * Returns the typed item in the owning list view that this entry is currently assigned to represent.
@@ -88,8 +90,9 @@ public:
 	 * @param OutItem The typed item.
 	 */
 	UFUNCTION(BlueprintCallable, Category="GUIS",
-			  meta = (ExpandBoolAsExecs = "ReturnValue", DeterminesOutputType = "DesiredClass", DynamicOutputParam = "OutItemObject", DefaultToSelf = UserObjectListEntry, DisplayName = "Get List item Object"))
-	static bool GetTypedListItemSafely(TScriptInterface<IUserObjectListEntry> UserObjectListEntry, TSubclassOf<UObject> DesiredClass, UObject *&OutItem);
+		meta = (ExpandBoolAsExecs = "ReturnValue", DeterminesOutputType = "DesiredClass", DynamicOutputParam = "OutItemObject", DefaultToSelf = UserObjectListEntry, DisplayName =
+			"Get List item Object"))
+	static bool GetTypedListItemSafely(TScriptInterface<IUserObjectListEntry> UserObjectListEntry, TSubclassOf<UObject> DesiredClass, UObject*& OutItem);
 
 private:
 	static int32 InputSuspensions;
