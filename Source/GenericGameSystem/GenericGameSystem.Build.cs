8cf4d073595bd0b685de6a94b81d67ac998092bc
luochuanyuewu <luochuanyuewu@gmail.com>
2025-07-22 13:20:03 +0800

    update

--- Source/GenericGameSystem/GenericGameSystem.Build.cs
+++ Source/GenericGameSystem/GenericGameSystem.Build.cs
@@ -24,7 +24,6 @@ public class GenericGameSystem : ModuleRules
                 "SlateCore",
                 "GameplayTags",
                 "UMG",
-                "StructUtils",
                 "TargetingSystem",
                 "GameplayTasks",
                 "GameplayAbilities",
